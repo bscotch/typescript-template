bc14045b14b60169b00d7ca2d42054decd14d56f
Adam Coster <adam@bscotch.net>
2021-05-21 16:36:22 -0500

    feat: Update all deps.

--- src/cli/cli-subcommand.ts
+++ src/cli/cli-subcommand.ts
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
-import cli from "commander";
+import cli from 'commander';
 
 // Kick it off
-cli.description('CLI Name: Subcommand')
-  .requiredOption("-r --required <info>", "Some required info")
-  .option("-o --optional <data>","Optional info",'default value')
-  .option("--flag","A boolean flag.")
+cli
+  .description('CLI Name: Subcommand')
+  .requiredOption('-r --required <info>', 'Some required info')
+  .option('-o --optional <data>', 'Optional info', 'default value')
+  .option('--flag', 'A boolean flag.')
   .parse();
 
 // Sample user options available via:
-cli.optional;
-cli.required;
-cli.flag; // false if not provided by the user, else true
+const options = cli.opts();
+options.optional;
+options.required;
+options.flag; // false if not provided by the user, else true
